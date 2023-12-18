cd478986fc746408961d63ab346e8c9a3f601752
Dh-Pardeep <pardeepjangra514@gmail.com>
2023-12-18 11:09:16 +0530

    todas task done

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import CardMapFirst from './components/CardMapFirst';
+import TodoApp from './components/TodoApp';
 
 function App() {
   return (
     <>
-      <CardMapFirst />
+      {/* <CardMapFirst /> */}
+      <TodoApp/>
     </>
   );
 }
